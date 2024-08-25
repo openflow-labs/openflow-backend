fb7326412fe787f71b7a8c29a39a2065a4e50679
nicoacosta.eth <nicoacosta8@gmail.com>
2024-08-25 01:00:16 -0300

    add lighthouse sdk

--- src/services/ipfsService.js
+++ src/services/ipfsService.js
@@ -1,12 +1,11 @@
-import { ipfs as _ipfs } from '../config/config';
+import { ipfs as _ipfs } from '../config/config'
 
 async function uploadFile(fileBuffer) {
-    const { create } = await import('ipfs-http-client');
-    const ipfs = create({ url: _ipfs.apiUrl });
-  
-    const { path } = await ipfs.add(fileBuffer);
-    return path;
-  }
-  
-  export default { uploadFile };
-  
+	const { create } = await import('ipfs-http-client')
+	const ipfs = create({ url: _ipfs.apiUrl })
+
+	const { path } = await ipfs.add(fileBuffer)
+	return path
+}
+
+export default { uploadFile }
